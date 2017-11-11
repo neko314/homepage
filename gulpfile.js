2de6ebe8a987643e99fcdedc9c5ffa4aaf244b04
Naoto Kaneko <naoty.k@gmail.com>
2017-11-11 20:46:12 +0900

    Install normalize.css

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
 let gulp = require("gulp");
 
 gulp.task("default", () => {
-  let srcFiles = ["stylesheets/index.css"];
+  let srcFiles = [
+    "node_modules/normalize.css/normalize.css",
+    "stylesheets/index.css"
+  ];
 
   return gulp.src(srcFiles)
     .pipe(gulp.dest("./public"));
