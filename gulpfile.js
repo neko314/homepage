60943d1dece38ad8299854e99421d77dc0c4d9df
Naoto Kaneko <naoty.k@gmail.com>
2017-11-13 20:41:58 +0900

    Build posts

--- gulpfile.js
+++ gulpfile.js
@@ -9,12 +9,20 @@ let markdown    = require("gulp-markdown");
 let sass        = require("gulp-sass");
 let sourcemaps  = require("gulp-sourcemaps");
 
-gulp.task("pages", () => {
-  gulp.src("./contents/**/*.md")
+gulp.task("homepage", () => {
+  gulp.src("./contents/index.md")
     .pipe(markdown())
     .pipe(layout("./layouts/index.ejs"))
     .pipe(htmlmin({ collapseWhitespace: true }))
     .pipe(gulp.dest("./public"));
+});
+
+gulp.task("post", () => {
+  gulp.src("./contents/posts/*.md")
+    .pipe(markdown())
+    .pipe(layout("./layouts/posts/post.ejs"))
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest("./public/posts"));
 });
 
 gulp.task("stylesheets", () => {
@@ -41,8 +49,10 @@ gulp.task("browser-sync", () => {
   })
 });
 
-gulp.task("default", ["pages", "stylesheets", "browser-sync"], () => {
-  gulp.watch("./contents/**/*.md", ["pages"]).on("change", browserSync.reload);
-  gulp.watch("./layouts/**/*.ejs", ["pages"]).on("change", browserSync.reload);
+gulp.task("default", ["homepage", "post", "stylesheets", "browser-sync"], () => {
+  gulp.watch("./contents/index.md", ["homepage"]).on("change", browserSync.reload);
+  gulp.watch("./contents/posts/*.md", ["post"]).on("change", browserSync.reload);
+  gulp.watch("./layouts/index.ejs", ["homepage"]).on("change", browserSync.reload);
+  gulp.watch("./layouts/posts/post.ejs", ["post"]).on("change", browserSync.reload);
   gulp.watch("./stylesheets/**/*.sass", ["stylesheets"]);
 });
