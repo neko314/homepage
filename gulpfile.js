4f0b9d5264923e93bb2df0dfe3b310702dbdc3fd
Naoto Kaneko <naoty.k@gmail.com>
2017-11-19 22:57:53 +0900

    Set post title from front matter
    When post title is set from filename, URLs percent-encoded aren't
    worked well because the encoding for filenames is incompatible in
    macOS.

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ let browserSync = require("browser-sync").create();
 let clean       = require("gulp-clean-css");
 let concat      = require("gulp-concat");
 let data        = require("gulp-data");
+let frontMatter = require("gulp-front-matter");
 let gulp        = require("gulp");
 let htmlmin     = require("gulp-htmlmin");
 let index       = require("gulp-ejs-index");
@@ -27,8 +28,9 @@ gulp.task("homepage", () => {
 
 gulp.task("posts", () => {
   gulp.src("./contents/posts/*.md")
+    .pipe(frontMatter({ property: "data" }))
     .pipe(markdown())
-    .pipe(index("./layouts/posts/index.ejs", { path: path }))
+    .pipe(index("./layouts/posts/index.ejs"))
     .pipe(data((file) => {
       return {
         pageTitle: "Posts - Naoto Kaneko"
@@ -41,12 +43,12 @@ gulp.task("posts", () => {
 
 gulp.task("post", () => {
   gulp.src("./contents/posts/*.md")
+    .pipe(frontMatter({ property: "data" }))
     .pipe(markdown())
     .pipe(data((file) => {
       let postTitle = path.basename(file.path, ".html");
       return {
-        pageTitle: `${postTitle} - Naoto Kaneko`,
-        postTitle: postTitle,
+        pageTitle: `${file.data.title} - Naoto Kaneko`,
         publishTime: file.stat.mtime.toLocaleString(),
         publishTimeISO8601: file.stat.mtime.toISOString()
       };
