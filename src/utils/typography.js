f2cda60ffd5ed0256b5ab55bffe0d6b540715b1d
Naoto Kaneko <naoty.k@gmail.com>
2018-08-26 20:07:05 +0900

    Setup typography with typograph.js

---
+++ src/utils/typography.js
@@ -0,0 +1,27 @@
+import Typography from "typography";
+
+const romanFontFamily = [
+  "-apple-system",
+  "BlinkMacSystemFont",
+  "Segoe UI",
+  "Roboto",
+  "Oxygen",
+  "Ubuntu",
+  "Cantarell",
+  "Fira Sans",
+  "Droid Sans",
+  "Helvetica Neue",
+];
+const japaneseFontFamily = [
+  "YuGothic",
+  "Yu Gothic",
+  "Hiragino Kaku Gothic ProN",
+  "Meiryo"
+];
+
+const typography = new Typography({
+  headerFontFamily: [...romanFontFamily, ...japaneseFontFamily, "sans-serif"],
+  bodyFontFamily: [...romanFontFamily, ...japaneseFontFamily, "sans-serif"]
+});
+
+export default typography;
