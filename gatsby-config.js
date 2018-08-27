57336aeec59deca1869ceb94bea28d94fb1a9327
Naoto Kaneko <naoty.k@gmail.com>
2018-08-27 20:07:55 +0900

    Setup gatsby-source-filesystem for markdown files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,12 @@
 module.exports = {
   plugins: [
     {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        path: "src/contents/posts/"
+      }
+    },
+    {
       resolve: "gatsby-plugin-typography",
       options: {
         pathToConfigModule: "src/utils/typography"
