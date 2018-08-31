427381917a85c3a67fcfcee8bc4456bff4e3b94e
Naoto Kaneko <naoty.k@gmail.com>
2018-08-31 10:45:38 +0900

    Add head tags to /posts/*.html with Head component

--- src/templates/post.jsx
+++ src/templates/post.jsx
@@ -1,5 +1,7 @@
 import React from "react";
+import path from "path";
 import Container from "../components/Container";
+import Head from "../components/Head";
 import Navigation from "../components/Navigation";
 import PageTitle from "../components/PageTitle";
 import PostContent from "../components/PostContent";
@@ -10,23 +12,40 @@ const links = [
   { title: "ブログ", path: "/posts/" }
 ];
 
-export default ({ data }) => (
-  <Container>
-    <PageTitle>{data.markdownRemark.frontmatter.title}</PageTitle>
-    <PostMetadata time={data.markdownRemark.frontmatter.time} />
-    <PostContent html={data.markdownRemark.html} />
-    <Navigation links={links} />
-  </Container>
-);
+export default ({ data }) => {
+  const postID = path.basename(data.markdownRemark.fileAbsolutePath, ".md");
+
+  return (
+    <Container>
+      <Head
+        title={data.markdownRemark.frontmatter.title}
+        description={data.markdownRemark.frontmatter.description || data.markdownRemark.frontmatter.title}
+        url={`${data.site.siteMetadata.rootURL}/${postID}.html`}
+        imageURL={`${data.site.siteMetadata.rootURL}/icons/256x256.png`}
+      />
+
+      <PageTitle>{data.markdownRemark.frontmatter.title}</PageTitle>
+      <PostMetadata time={data.markdownRemark.frontmatter.time} />
+      <PostContent html={data.markdownRemark.html} />
+      <Navigation links={links} />
+    </Container>
+  )
+};
 
 export const query = graphql`
   query PostQuery($id: String!) {
+    site {
+      siteMetadata {
+        rootURL
+      }
+    }
     markdownRemark(id: { eq: $id }) {
       frontmatter {
         title
         time
       }
       html
+      fileAbsolutePath
     }
   }
 `;
