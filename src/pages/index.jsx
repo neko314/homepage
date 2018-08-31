bd7cf3bfa0e5a690700523056ed74fbffa3eebcc
Naoto Kaneko <naoty.k@gmail.com>
2018-08-31 10:17:23 +0900

    Add head tags to / with Head component

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import Link from "gatsby-link";
 import Container from "../components/Container";
+import Head from "../components/Head";
 import List from "../components/List";
 import PageTitle from "../components/PageTitle";
 
-export default () => (
+export default ({ data }) => (
   <Container>
+    <Head
+      title="Naoto Kaneko"
+      description="Naoto Kaneko's homepage"
+      url={data.site.siteMetadata.rootURL}
+      imageURL={`${data.site.siteMetadata.rootURL}/icons/256x256.png`}
+    />
+
     <PageTitle>Naoto Kaneko</PageTitle>
     <List>
       <li><Link to="/posts/">ブログ</Link></li>
@@ -14,3 +22,13 @@ export default () => (
     </List>
   </Container>
 );
+
+export const query = graphql`
+  query TopQuery {
+    site {
+      siteMetadata {
+        rootURL
+      }
+    }
+  }
+`;
