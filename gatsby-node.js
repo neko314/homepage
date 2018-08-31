93e1ea35955638696524c5603da663ce6c220fea
Naoto Kaneko <naoty.k@gmail.com>
2018-08-31 18:23:54 +0900

    Rewrite gatsby-node.js to use await

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,31 +1,30 @@
 const path = require("path");
 
-exports.createPages = ({ graphql, boundActionCreators }) => {
+exports.createPages = async ({ graphql, boundActionCreators }) => {
   const { createPage } = boundActionCreators;
-  return new Promise((resolve, reject) => {
-    graphql(`
-      {
-        allMarkdownRemark {
-          edges {
-            node {
-              id
-              fileAbsolutePath
-            }
+  const result = await graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            id
+            fileAbsolutePath
           }
         }
       }
-    `).then(result => {
-      result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-        const basename = path.basename(node.fileAbsolutePath, ".md");
-        createPage({
-          path: `/posts/${basename}.html`,
-          component: path.resolve("./src/templates/post.jsx"),
-          context: {
-            id: node.id
-          }
-        });
-      });
-      resolve();
-    })
+    }
+  `);
+
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    const basename = path.basename(node.fileAbsolutePath, ".md");
+    createPage({
+      path: `/posts/${basename}.html`,
+      component: path.resolve("./src/templates/post.jsx"),
+      context: {
+        id: node.id
+      }
+    });
   });
+
+  return Promise.resolve();
 };
