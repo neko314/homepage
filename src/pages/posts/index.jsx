569cdff94b718d8eabd806612e95a7f16d6c014b
Naoto Kaneko <naoty.k@gmail.com>
2018-08-31 10:21:34 +0900

    Add head tags to /posts/ with Head component

--- src/pages/posts/index.jsx
+++ src/pages/posts/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import path from "path";
 import Link from "gatsby-link";
 import Container from "../../components/Container";
+import Head from "../../components/Head";
 import List from "../../components/List";
 import Navigation from "../../components/Navigation";
 import PageTitle from "../../components/PageTitle";
@@ -12,6 +13,13 @@ const links = [
 
 export default ({ data }) => (
   <Container>
+    <Head
+      title="Naoto Kaneko's posts"
+      description="Naoto Kaneko's posts"
+      url={`${data.site.siteMetadata.rootURL}/posts/`}
+      imageURL={`${data.site.siteMetadata.rootURL}/icons/256x256.png`}
+    />
+
     <PageTitle>ブログ</PageTitle>
     <List>
       {data.allMarkdownRemark.edges.map(({ node }, index) => (
@@ -28,6 +36,11 @@ export default ({ data }) => (
 
 export const query = graphql`
   query PostsQuery {
+    site {
+      siteMetadata {
+        rootURL
+      }
+    }
     allMarkdownRemark(sort: { fields: [frontmatter___time], order: DESC }) {
       edges {
         node {
