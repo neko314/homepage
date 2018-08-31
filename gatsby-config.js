9695469cee45c8dcff4dc1c5e9d35a71ec2bc2d4
Naoto Kaneko <naoty.k@gmail.com>
2018-08-31 10:15:59 +0900

    Add root URL to site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  siteMetadata: {
+    rootURL: process.env.NODE_ENV == "production" ? "https://naoty.github.io" : "http://localhost:8000"
+  },
   plugins: [
     {
       resolve: "gatsby-source-filesystem",
