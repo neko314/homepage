856b0a444b668bb5c0cbcb74a7d5a0af9fab14e3
Naoto Kaneko <naoty.k@gmail.com>
2019-09-20 19:54:59 +0900

    Remove all files except for contents

--- src/components/PageTitle.jsx
+++
@@ -1,8 +0,0 @@
-import styled from "@emotion/styled"
-import typography from "../utils/typography";
-
-export default styled.h1`
-  margin-bottom: ${typography.rhythm(1/2)};
-  padding-bottom: ${typography.rhythm(1/4)};
-  border-bottom: 1px solid #333;
-`;
