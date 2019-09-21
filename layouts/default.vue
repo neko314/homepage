ad7ab74989e3b0f3286d47d53e1428cef0ee9ef9
Naoto Kaneko <naoty.k@gmail.com>
2019-09-21 16:29:44 +0900

    Add default layout

---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <main>
+    <nuxt />
+  </main>
+</template>
+
+<style>
+main {
+  background-color: #fff;
+  padding: 1rem;
+}
+
+@media (min-width: 36rem) {
+  main {
+    width: 36rem;
+    margin: 2rem auto;
+    padding: 2rem;
+    box-shadow: 0 5px 5px rgba(0, 0, 0, 0.1), 3px 0 5px rgba(0, 0, 0, 0.1),
+      -3px 0 5px rgba(0, 0, 0, 0.1);
+  }
+}
+</style>
