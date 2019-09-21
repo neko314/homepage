9f7f38d2be1f070dde858a68d7b424d713796457
Naoto Kaneko <naoty.k@gmail.com>
2019-09-21 16:14:03 +0900

    Add tags in head

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,43 @@
 export default {
-  css: ["normalize.css/normalize.css", "assets/global.css"]
+  css: ["normalize.css/normalize.css", "assets/global.css"],
+  head: {
+    title: "",
+    meta: [
+      { charset: "utf-8" },
+      {
+        name: "viewport",
+        content: "width=device-width, initial-scale=1, shrink-to-fit=no"
+      },
+      { hid: "description", name: "description", content: "" },
+      { hid: "theme-color", name: "theme-color", content: "white" },
+      { hid: "og:type", property: "og:type", content: "profile" },
+      { hid: "og:title", property: "og:title", content: "" },
+      { hid: "og:description", property: "og:description", content: "" },
+      { hid: "og:url", property: "og:url", content: "" },
+      { hid: "og:image", property: "og:image", content: "" },
+      { hid: "twitter:card", property: "twitter:card", content: "summary" },
+      { hid: "twitter:site", property: "twitter:site", content: "@naoty_k" },
+      { hid: "twitter:title", property: "twitter:title", content: "" },
+      {
+        hid: "twitter:description",
+        property: "twitter:description",
+        content: ""
+      }
+    ],
+    link: [
+      {
+        hid: "icon",
+        rel: "icon",
+        type: "image/png",
+        href: "/icons/favicon.png"
+      },
+      {
+        hid: "apple-touch-icon",
+        rel: "apple-touch-icon",
+        href: "/icons/apple-touch-icon.png"
+      },
+      { hid: "canonical", rel: "canonical", href: "" },
+      { hid: "manifest", rel: "manifest", href: "/manifest.json" }
+    ]
+  }
 };
