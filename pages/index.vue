254d10a428fa76c2d72a16de20a84572616d2c84
Naoto Kaneko <naoty.k@gmail.com>
2019-09-21 16:51:16 +0900

    Add links on top page

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,19 @@
 <template>
-  <h1>Naoto Kaneko</h1>
+  <div>
+    <h1>Naoto Kaneko</h1>
+    <ul>
+      <li>
+        <nuxt-link to="/posts/">Posts</nuxt-link>
+      </li>
+      <li>
+        <a href="https://github.com/naoty">GitHub</a>
+      </li>
+      <li>
+        <a href="https://twitter.com/naoty_k">Twitter</a>
+      </li>
+      <li>
+        <a href="https://atcoder.jp/users/naoty">AtCoder</a>
+      </li>
+    </ul>
+  </div>
 </template>
